3452829ce36fa5fe3ada028320e93d969a7b25b5
Artur <aghartur@gmail.com>
2023-03-11 17:54:59 +0100

    starter commit storybook settings

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { App } from '../App';
+
+export default {
+  title: 'Example/App',
+  component: App,
+  
+} as ComponentMeta<typeof App>;
+
+
+const Template: ComponentStory<typeof App> = () => <App />;
+
+export const Primary = Template.bind({});
